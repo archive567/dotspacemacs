ebf4978776695ad65b6bd60019a6122441e6369b
Tony Day <tonyday567@gmail.com>
2017-10-21 10:55:04 +1000

    minor change

--- init.el
+++ init.el
@@ -3,9 +3,8 @@
 ;; It must be stored in your home directory.
 
 (defun dotspacemacs/layers ()
-  "Configuration Layers declaration.
-You should not put any user code in this function besides modifying the variable
-values."
+  "Layer configuration:
+This function should only modify configuration layer settings."
   (setq-default
    ;; Base distribution to use. This is a layer contained in the directory
    ;; `+distribution'. For now available distributions are `spacemacs-base'
@@ -104,11 +103,10 @@ values."
    dotspacemacs-install-packages 'used-only))
 
 (defun dotspacemacs/init ()
-  "Initialization function.
-This function is called at the very startup of Spacemacs initialization
-before layers configuration.
-You should not put any user code in there besides modifying the variable
-values."
+  "Initialization:
+This function is called at the very beginning of Spacemacs startup,
+before layer configuration.
+It should only modify the values of Spacemacs settings."
   ;; This setq-default sexp is an exhaustive list of all the supported
   ;; spacemacs settings.
   (setq-default
@@ -128,7 +126,7 @@ values."
    dotspacemacs-check-for-update nil
    ;; If non-nil, a form that evaluates to a package directory. For example, to
    ;; use different package directories for different Emacs versions, set this
-   ;; to `emacs-version'.
+   ;; to `emacs-version'. (default nil)
    dotspacemacs-elpa-subdirectory nil
    ;; One of `vim', `emacs' or `hybrid'.
    ;; `hybrid' is like `vim' except that `insert state' is replaced by the
@@ -149,7 +147,7 @@ values."
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
    ;; Possible values for list-type are:
-   ;; `recents' `bookmarks' `projects' `agenda' `todos'."
+   ;; `recents' `bookmarks' `projects' `agenda' `todos'.
    ;; List sizes may be nil, in which case
    ;; `spacemacs-buffer-startup-lists-length' takes effect.
    dotspacemacs-startup-lists '((projects . 10) agenda (recents . 5))
@@ -171,7 +169,7 @@ values."
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
-   ;; The leader key
+   ;; The leader key (default "SPC")
    dotspacemacs-leader-key "SPC"
    ;; The key used for Emacs commands `M-x' (after pressing on the leader key).
    ;; (default "SPC")
@@ -214,7 +212,7 @@ values."
    ;; start. (default nil)
    dotspacemacs-auto-resume-layouts nil
    ;; If non-nil, auto-generate layout name when creating new layouts. Only has
-   ;; effect when using the "jump to layout by number" commands.
+   ;; effect when using the "jump to layout by number" commands. (default nil)
    dotspacemacs-auto-generate-layout-names nil
    ;; Size (in MB) above which spacemacs will prompt to open the large file
    ;; literally to avoid performance issues. Opening a file literally means that
@@ -240,8 +238,9 @@ values."
    ;; source settings. Else, disable fuzzy matching in all sources.
    ;; (default 'always)
    dotspacemacs-helm-use-fuzzy 'always
-   ;; If non-nil the paste micro-state is enabled. When enabled pressing `p'
-   ;; several times cycle between the kill ring content. (default nil)
+   ;; If non-nil, the paste transient-state is enabled. While enabled, pressing
+   ;; `p' several times cycles through the elements in the `kill-ring'.
+   ;; (default nil)
    dotspacemacs-enable-paste-transient-state nil
    ;; Which-key delay in seconds. The which-key buffer is the popup listing
    ;; the commands bound to the current keystroke sequence. (default 0.4)
@@ -344,6 +343,7 @@ values."
    ;; %n - Narrow if appropriate
    ;; %z - mnemonics of buffer, terminal, and keyboard coding systems
    ;; %Z - like %z, but including the end-of-line format
+   ;; (default "%I@%S")
    dotspacemacs-frame-title-format "%I@%S"
    ;; Format specification for setting the icon title format
    ;; (default nil - same as frame-title-format)
@@ -357,15 +357,18 @@ values."
    ;; Either nil or a number of seconds. If non-nil zone out after the specified
    ;; number of seconds. (default nil)
    dotspacemacs-zone-out-when-idle nil
+   ;; Run `spacemacs/prettify-org-buffer' when
+   ;; visiting README.org files of Spacemacs.
+   ;; (default nil)
+   dotspacemacs-pretty-docs nil
    ))
 
 (defun dotspacemacs/user-init ()
-  "Initialization function for user code.
-It is called immediately after `dotspacemacs/init', before layer configuration
-executes.
- This function is mostly useful for variables that need to be set
-before packages are loaded. If you are unsure, you should try in setting them in
-`dotspacemacs/user-config' first."
+  "Initialization for user code:
+This function is called immediately after `dotspacemacs/init', before layer
+configuration.
+It is mostly for variables that should be set before packages are loaded.
+If you are unsure, try setting them in `dotspacemacs/user-config' first."
   )
 
 (defun dotspacemacs/user-config ()
@@ -379,7 +382,7 @@ you should place your code here."
   ;; fixme: add to haskell-ext
   (bind-key* "M-n" 'flycheck-next-error)
   (bind-key* "M-p" 'flycheck-previous-error)
-  (spacemacs/add-flycheck-hook 'literate-haskell-mode)
+  ;; (spacemacs/add-flycheck-hook 'literate-haskell-mode)
 
   (evil-leader/set-key
     "wa" 'default-frame
@@ -712,3 +715,242 @@ you should place your code here."
  '(default ((t (:background nil))))
  '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
  '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(css-indent-offset 2)
+ '(custom-safe-themes
+   (quote
+    ("e8825f26af32403c5ad8bc983f8610a4a4786eb55e3a363fa9acb48e0677fe7e" default)))
+ '(dired-auto-revert-buffer t)
+ '(evil-escape-key-sequence "jk")
+ '(flycheck-display-errors-function (quote flycheck-display-error-messages))
+ '(flycheck-pos-tip-mode nil)
+ '(haskell-font-lock-symbols t)
+ '(haskell-hayoo-url "http://hayoo.fh-wedel.de/?query=%s")
+ '(haskell-hoogle-url "https://www.stackage.org/lts/hoogle/?q=%s")
+ '(haskell-indent-spaces 4)
+ '(haskell-indentation-layout-offset 4)
+ '(haskell-indentation-left-offset 4)
+ '(haskell-indentation-starter-offset 2)
+ '(haskell-indentation-where-post-offset 2)
+ '(haskell-indentation-where-pre-offset 2)
+ '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
+ '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")) t)
+ '(ido-auto-merge-delay-time 4)
+ '(org-M-RET-may-split-line (quote ((headline . t))))
+ '(org-adapt-indentation nil)
+ '(org-agenda-compact-blocks t)
+ '(org-agenda-custom-commands
+   (quote
+    (("n" "Agenda and all TODO's"
+      ((agenda ""
+               ((org-agenda-overriding-header "today")))
+       (tags "REFILE"
+             ((org-agenda-overriding-header "refile me pls")
+              (org-tags-match-list-sublevels nil)))
+       (todo "+TODO='NEXT'" nil)
+       (todo "+TODO='TODO'" nil))
+      nil)
+     ("z" "draft full agenda"
+      ((agenda ""
+               ((org-agenda-overriding-header "today")))
+       (tags "REFILE"
+             ((org-agenda-overriding-header "refiles")))
+       (tags "TODO=\"TODO\"+SCHEDULED>\"<today>\""
+             ((org-agenda-overriding-header "scheduled")))
+       (tags "TODO=\"TODO\"-SCHEDULED<=\"<today>\"-SCHEDULED>\"<today>\""
+             ((org-agenda-overriding-header "the rest")))
+       (tags "TODO=\"DONE\""
+             ((org-agenda-overriding-header "done and dusted"))))
+      nil nil)
+     ("x" "sandpit" tags "+SCHEDULED>=\"<2008-10-11>\"" nil))))
+ '(org-agenda-diary-file (quote diary-file))
+ '(org-agenda-dim-blocked-tasks nil)
+ '(org-agenda-files
+   (quote
+    ("~/org/strategy.org" "~/org/econ.org" "~/org/cat.org" "~/org/nsteel.org" "~/org/haskell.org" "~/org/sys.org" "~/org/stuff.org" "~/org/refile.org" "~/org/life.org" "~/org/emacs.org" "~/org/bugz.org")))
+ '(org-agenda-include-diary nil)
+ '(org-agenda-ndays 1)
+ '(org-agenda-persistent-filter t)
+ '(org-agenda-scheduled-leaders (quote ("" "S%d: ")))
+ '(org-agenda-skip-additional-timestamps-same-entry t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-deadline-is-shown t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-skip-timestamp-if-done t)
+ '(org-agenda-skip-unavailable-files t)
+ '(org-agenda-sorting-strategy
+   (quote
+    ((agenda habit-down time-up todo-state-up user-defined-up priority-down effort-up category-keep)
+     (todo category-up priority-down effort-up)
+     (tags category-up priority-down effort-up)
+     (search category-keep))))
+ '(org-agenda-span (quote day))
+ '(org-agenda-start-on-weekday nil)
+ '(org-agenda-tags-column -102)
+ '(org-agenda-tags-todo-honor-ignore-options t)
+ '(org-agenda-text-search-extra-files nil)
+ '(org-agenda-use-time-grid nil)
+ '(org-agenda-window-setup (quote current-window))
+ '(org-babel-load-languages
+   (quote
+    ((emacs-lisp . t)
+     (sh . t)
+     (dot . t)
+     (org . t)
+     (js . t)
+     (haskell \.t))))
+ '(org-babel-results-keyword "results")
+ '(org-capture-bookmark nil)
+ '(org-capture-templates
+   (quote
+    (("r" "refile" entry
+      (file "~/org/refile.org")
+      "* TODO %?
+")
+     ("z" "bugz" entry
+      (file+headline "~/org/bugz.org" "bugz!")
+      "* TODO %?
+%a"))))
+ '(org-clock-clocked-in-display (quote mode-line))
+ '(org-clock-idle-time 10)
+ '(org-completion-use-ido t)
+ '(org-confirm-babel-evaluate nil)
+ '(org-confirm-elisp-link-function nil)
+ '(org-crypt-disable-auto-save nil)
+ '(org-crypt-key "F0B66B40")
+ '(org-cycle-global-at-bob t)
+ '(org-cycle-separator-lines 0)
+ '(org-deadline-warning-days 5)
+ '(org-edit-src-content-indentation 0)
+ '(org-enable-priority-commands nil)
+ '(org-enforce-todo-dependencies t)
+ '(org-entities-user
+   (quote
+    (("hardbreak" "\\\\" nil "" "" "" "")
+     ("greytext" "\\\\color{greytext}" nil "" "" "" ""))))
+ '(org-export-babel-evaluate nil)
+ '(org-export-copy-to-kill-ring t)
+ '(org-export-date-timestamp-format "%Y-%m-%d")
+ '(org-export-html-inline-images t)
+ '(org-export-html-link-home "www.scarcecapital.com")
+ '(org-export-html-postamble nil)
+ '(org-export-html-style "")
+ '(org-export-html-style-extra "")
+ '(org-export-html-style-include-default nil)
+ '(org-export-html-xml-declaration
+   (quote
+    (("html" . "")
+     ("was-html" . "<?xml version=\"1.0\" encoding=\"%s\"?>")
+     ("php" . "<?php echo \"<?xml version=\\\"1.0\\\" encoding=\\\"%s\\\" ?>\"; ?>"))))
+ '(org-export-with-drawers nil)
+ '(org-export-with-tags nil)
+ '(org-export-with-timestamps nil)
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-footnote-section nil)
+ '(org-from-is-user-regexp "\\<tony day\\>")
+ '(org-hide-leading-stars nil)
+ '(org-html-head-include-default-style nil)
+ '(org-html-head-include-scripts nil)
+ '(org-html-htmlize-output-type (quote css))
+ '(org-html-postamble nil)
+ '(org-insert-heading-respect-content nil)
+ '(org-latex-to-pdf-process
+   (quote
+    ("xelatex -interaction nonstopmode -output-directory %o %f" "xetex %f")))
+ '(org-link-abbrev-alist (quote (("google" . "http://www.google.com/search?q="))))
+ '(org-log-done nil)
+ '(org-log-into-drawer "LOGBOOK")
+ '(org-modules (quote (org-id org-info)))
+ '(org-outline-path-complete-in-steps nil)
+ '(org-provide-todo-statistics nil)
+ '(org-refile-allow-creating-parent-nodes (quote confirm))
+ '(org-refile-target-verify-function nil)
+ '(org-refile-targets (quote ((org-agenda-files :maxlevel . 2))))
+ '(org-refile-use-outline-path (quote file))
+ '(org-remove-highlights-with-change nil)
+ '(org-return-follows-link t)
+ '(org-show-notification-handler (quote alert))
+ '(org-src-fontify-natively t)
+ '(org-src-lang-modes
+   (quote
+    (("plantuml" . fundamental)
+     ("ocaml" . tuareg)
+     ("elisp" . emacs-lisp)
+     ("ditaa" . artist)
+     ("asymptote" . asy)
+     ("dot" . graphviz-dot)
+     ("sqlite" . sql)
+     ("calc" . fundamental)
+     ("C" . c)
+     ("cpp" . c++)
+     ("screen" . shell-script)
+     ("R" . R)
+     ("haskell" . haskell))))
+ '(org-src-preserve-indentation t)
+ '(org-startup-folded t)
+ '(org-startup-indented t)
+ '(org-support-shift-select t)
+ '(package-archives
+   (quote
+    (("melpa" . "http://melpa.org/packages/")
+     ("org" . "http://orgmode.org/elpa/")
+     ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (hasky-stack white-sand-theme symon string-inflection rebecca-theme password-generator org-brain impatient-mode helm-purpose window-purpose imenu-list exotica-theme evil-org evil-lion editorconfig dante browse-at-remote multiple-cursors hasky-extensions idris-mode prop-menu zonokai-theme zenburn-theme zen-and-art-theme yaml-mode xterm-color ws-butler winum which-key web-mode volatile-highlights vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme toc-org tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme spaceline powerline spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode sass-mode reverse-theme reveal-in-osx-finder restart-emacs rainbow-delimiters railscasts-theme purple-haze-theme pug-mode professional-theme popwin planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode pcre2el pbcopy pastels-on-dark-theme paradox spinner osx-trash osx-dictionary orgit organic-green-theme org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-plus-contrib org-download org-bullets open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme noctilux-theme niflheim-theme neotree naquadah-theme mustang-theme multi-term move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minimal-theme material-theme markdown-toc markdown-mode majapahit-theme magit-gitflow madhat2r-theme macrostep lush-theme lorem-ipsum linum-relative link-hint light-soap-theme less-css-mode launchctl jbeans-theme jazz-theme ir-black-theme intero inkpot-theme info+ indent-guide hydra hungry-delete htmlize hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile helm-hoogle helm-gitignore helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme haskell-snippets haml-mode gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md gandalf-theme fuzzy flycheck-pos-tip pos-tip flycheck-haskell flycheck pkg-info epl flx-ido flx flatui-theme flatland-theme firebelly-theme fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit magit-popup git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight espresso-theme eshell-z eshell-prompt-extras esh-help emmet-mode elisp-slime-nav ein skewer-mode request-deferred websocket request deferred js2-mode simple-httpd dumb-jump f s dracula-theme django-theme diminish diff-hl darktooth-theme autothemer darkokai-theme darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme company-web web-completion-data company-statistics company-ghci company-ghc ghc haskell-mode company-cabal company column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cmm-mode clues-theme clean-aindent-mode cherry-blossom-theme busybee-theme bubbleberry-theme birds-of-paradise-plus-theme bind-map bind-key badwolf-theme auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed dash apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes aggressive-indent afternoon-theme adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup farmhouse-theme)))
+ '(paradox-automatically-star t)
+ '(paradox-execute-asynchronously t t)
+ '(paradox-github-token "9c0337a9d7ffbfa53d06bf294278dc777a2b77c9")
+ '(projectile-known-projects-file "~/.spacemacs.d/.cache/projectile-bookmarks.eld")
+ '(reb-re-syntax (quote read))
+ '(recentf-save-file "~/.spacemacs.d/.cache/recentf")
+ '(safe-local-variable-values (quote ((intero-targets "chart-unit:lib"))))
+ '(tags-add-tables nil)
+ '(tramp-persistency-file-name "~/.spacemacs.d/.cache/tramp" t)
+ '(vc-annotate-background "#ecf0f1")
+ '(vc-annotate-color-map
+   (quote
+    ((30 . "#e74c3c")
+     (60 . "#c0392b")
+     (90 . "#e67e22")
+     (120 . "#d35400")
+     (150 . "#f1c40f")
+     (180 . "#d98c10")
+     (210 . "#2ecc71")
+     (240 . "#27ae60")
+     (270 . "#1abc9c")
+     (300 . "#16a085")
+     (330 . "#2492db")
+     (360 . "#0a74b9"))))
+ '(vc-annotate-very-old-color "#0a74b9")
+ '(when
+      (or
+       (not
+        (boundp
+         (quote ansi-term-color-vector)))
+       (not
+        (facep
+         (aref ansi-term-color-vector 0)))))
+ '(yas-fallback-behavior (quote call-other-command))
+ '(yas-indent-line (quote nothing))
+ '(yas-prompt-functions (quote (yas-ido-prompt yas-dropdown-prompt)))
+ '(yas-triggers-in-field t)
+ '(yas-wrap-around-region t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
